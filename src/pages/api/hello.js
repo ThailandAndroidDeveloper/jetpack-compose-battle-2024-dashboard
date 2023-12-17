69f9da30ef0bc6290dd9f09c63287b2348762697
Thitare Nimanong <31370613+goodjobguy1234@users.noreply.github.com>
2023-12-18 00:40:58 +0700

    hover animation

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+
+  if (req.method == "POST") {
+    const payload = {
+      score: 10
+    }
+    // fetch()
+  } else {
+    res.status(200).json({ name: 'John Doe' })
+  }
 }
