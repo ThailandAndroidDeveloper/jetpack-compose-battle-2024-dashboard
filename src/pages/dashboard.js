7d52d42dac5750348222b1cc1cb498d157a152c8
Thitare Nimanong <31370613+goodjobguy1234@users.noreply.github.com>
2023-12-24 15:56:31 +0700

    hover animation (#1)

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,17 +1,129 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
-const rowComponent = () => {
+const tableHeaderContent = () => {
     return (<>
+        <tr>
+            <th scope="col" class="px-6 py-5 max-w-[10px]">
+                Rank
+            </th>
+            <th scope="col" class="px-6">
+                {/* for image */}
+            </th>
+            <th scope="col" class="px-6">
+                Name
+            </th>
+            <th scope="col" colSpan="3" class="px-6">
+                Complete
+            </th>
+            <th scope="col" colSpan="3" class="px-6">
+                Point
+                {/* <span class="sr-only">Edit</span> */}
+            </th>
+        </tr>
     </>)
 }
 
-const cardBody = () => {
-    return (<>
-    </>)
+const tableBodyContent = (data, setHoverIndex) => {
+    return data?.map((element, index) => {
+        return (
+            <>
+                <tbody className="group animate-in fade-in border-opacity-0 bg-gray-800 hover:bg-gray-700">
+                    <tr key={index} clasName="group-hover:bg-700 border-opacity-0 bg-white bg-gray-800 border-0"
+                        onMouseEnter={() => setHoverIndex(index)} onMouseLeave={() => setHoverIndex(-1)}
+                    >
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white max-w-[10px]" >
+                            {index + 1}
+                        </th>
+                        <td class="px-6 py-4 max-w-[50px]">
+                            {/* image */}
+                            <div class="avatar">
+                                <div class="w-24 rounded-full">
+                                    <img src="https://marketplace.canva.com/EAFHfL_zPBk/1/0/1600w/canva-yellow-inspiration-modern-instagram-profile-picture-kpZhUIzCx_w.jpg" />
+                                </div>
+                            </div>
+                            {/* <img src="https://marketplace.canva.com/EAFHfL_zPBk/1/0/1600w/canva-yellow-inspiration-modern-instagram-profile-picture-kpZhUIzCx_w.jpg" className="w-[100px] h-[100]" /> */}
+                        </td>
+                        <td class="px-6 py-4 max-w-[50px] overflow-ellipsis overflow-hidden">
+                            Temmies Sammmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+                        </td>
+                        <td class="px-6 py-4" colSpan="3">
+                            <div>4/7</div>
+                        </td>
+                        <td class="px-6 py-4 text-3xl" colSpan="3">
+                            10
+                            {/* <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a> */}
+                        </td>
+                    </tr>
+
+                    {/* hidden data */}
+                    <tr className="animate-in fade-in  dark:bg-gray-900">
+                        <td colSpan={12}>
+                            <div tabindex={index} className="collapse focus:outline-none" id={"hover-input-" + index}>
+                                {/* <input type="checkbox" id={"hover-input-"+index} />  */}
+                                <div className="collapse-content">
+                                    <div>
+                                        <h1>Score part 1: .....</h1>
+                                        <h1>Score part 2: .....</h1>
+                                        <h1>Score part 3: .....</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+
+                    {/* <tr class="animate-in fade-in bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"><td colSpan={12}>
+                    <button className="text-center w-full">expand</button>
+                </td></tr> */}
+                    {/* padding between data */}
+                    {/* {index < data.length - 1 &&
+                    <tr className="border-0"><td>
+                        <div className="mt-3"></div>
+                    </td></tr>
+                } */}
+                </tbody>
+                {index < data.length - 1 &&
+                    <tbody>
+                        <tr className="border-0"><td>
+                            <div className="mt-3"></div>
+                        </td></tr>
+                    </tbody>
+
+                }
+
+            </>
+        )
+    });
 }
 
+const shaffle = () => {
+
+}
+
+const createData = (number) => {
+    let arrs = []
+    for (let i = 0; i < number; i++) {
+        arrs.push(
+            {
+                name: "Thitare Nimanong",
+                complete: "5",
+                point: "30"
+            }
+        )
+    }
+    return arrs
+}
 
 export default function Dashboard() {
+    const [joiner, setJoiner] = useState(createData(50))
+    const [hoverIndex, setHoverIndex] = useState(-1)
+    useEffect(() => {
+        console.log("hover " + hoverIndex)
+        if (hoverIndex != -1) {
+            const hoverId = document.getElementById("hover-input-" + hoverIndex);
+            hoverId.focus()
+        }
+    }, [hoverIndex])
     return (
         <main className={`flex min-h-screen flex-col items-center`}>
             <div className="m-10">
@@ -19,175 +131,28 @@ export default function Dashboard() {
             </div>
 
             <div className="w-full mx-auto px-24">
-                <div class="relative overflow-x-auto h-[39rem] dark:bg-gray-700 shadow-md sm:rounded-lg">
-                    <table class="w-full text-left text-gray-500 dark:text-gray-400 overscroll-y-visible">
+                {/* dark:bg-gray-700 */}
+                <div class="relative overflow-x-auto h-[39rem] shadow-md sm:rounded-lg ">
+                    <table class="w-full text-left text-gray-500 dark:text-gray-400 overscroll-y-auto">
                         <thead className="text-xs sticky top-0 py-3 text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                            <tr>
-                                <th scope="col" class="px-6 py-5 max-w-[10px]">
-                                    Rank
-                                </th>
-                                <th scope="col" class="px-6">
-                                    {/* for image */}
-                                </th>
-                                <th scope="col" class="px-6">
-                                    Name
-                                </th>
-                                <th scope="col" colSpan="3" class="px-6">
-                                    Complete
-                                </th>
-                                <th scope="col" colSpan="3" class="px-6">
-                                    Point
-                                    {/* <span class="sr-only">Edit</span> */}
-                                </th>
-                            </tr>
+                            {tableHeaderContent()}
                         </thead>
 
                         <tbody>
-                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white max-w-[10px]" >
-                                    1
-                                </th>
-                                <td class="px-6 py-4 max-w-[50px]">
-                                    {/* image */}
-                                    <img src="https://marketplace.canva.com/EAFHfL_zPBk/1/0/1600w/canva-yellow-inspiration-modern-instagram-profile-picture-kpZhUIzCx_w.jpg" className="w-[100px] h-[100]"/>
-                                </td>
-                                <td class="px-6 py-4 max-w-[50px] overflow-ellipsis overflow-hidden">
-                                    Temmies Sammmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
-                                </td>
-                                <td class="px-6 py-4" colSpan="3">
-                                    4/7
-                                </td>
-                                <td class="px-6 py-4 text-3xl" colSpan="3">
-                                    10
-                                    {/* <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a> */}
-                                </td>
-                            </tr>
-
-                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white max-w-[10px]" >
-                                    1
-                                </th>
-                                <td class="px-6 py-4 max-w-[50px]">
-                                    {/* image */}
-                                    <img src="https://marketplace.canva.com/EAFHfL_zPBk/1/0/1600w/canva-yellow-inspiration-modern-instagram-profile-picture-kpZhUIzCx_w.jpg" className="w-[100px] h-[100]"/>
-                                </td>
-                                <td class="px-6 py-4 max-w-[50px] overflow-ellipsis overflow-hidden">
-                                    Temmies Sammmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
-                                </td>
-                                <td class="px-6 py-4" colSpan="3">
-                                    4/7
-                                </td>
-                                <td class="px-6 py-4 text-3xl" colSpan="3">
-                                    10
-                                    {/* <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a> */}
-                                </td>
-                            </tr>
-
-                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white max-w-[10px]" >
-                                    1
-                                </th>
-                                <td class="px-6 py-4 max-w-[50px]">
-                                    {/* image */}
-                                    <img src="https://marketplace.canva.com/EAFHfL_zPBk/1/0/1600w/canva-yellow-inspiration-modern-instagram-profile-picture-kpZhUIzCx_w.jpg" className="w-[100px] h-[100]"/>
-                                </td>
-                                <td class="px-6 py-4 max-w-[50px] overflow-ellipsis overflow-hidden">
-                                    Temmies Sammmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
-                                </td>
-                                <td class="px-6 py-4" colSpan="3">
-                                    4/7
-                                </td>
-                                <td class="px-6 py-4 text-3xl" colSpan="3">
-                                    10
-                                    {/* <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a> */}
-                                </td>
-                            </tr>
-
-                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white max-w-[10px]" >
-                                    1
-                                </th>
-                                <td class="px-6 py-4 max-w-[50px]">
-                                    {/* image */}
-                                    <img src="https://marketplace.canva.com/EAFHfL_zPBk/1/0/1600w/canva-yellow-inspiration-modern-instagram-profile-picture-kpZhUIzCx_w.jpg" className="w-[100px] h-[100]"/>
-                                </td>
-                                <td class="px-6 py-4 max-w-[50px] overflow-ellipsis overflow-hidden">
-                                    Temmies Sammmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
-                                </td>
-                                <td class="px-6 py-4" colSpan="3">
-                                    4/7
-                                </td>
-                                <td class="px-6 py-4 text-3xl" colSpan="3">
-                                    10
-                                    {/* <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a> */}
-                                </td>
-                            </tr>
-
-
-                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white max-w-[10px]" >
-                                    1
-                                </th>
-                                <td class="px-6 py-4 max-w-[50px]">
-                                    {/* image */}
-                                    <img src="https://marketplace.canva.com/EAFHfL_zPBk/1/0/1600w/canva-yellow-inspiration-modern-instagram-profile-picture-kpZhUIzCx_w.jpg" className="w-[100px] h-[100]"/>
-                                </td>
-                                <td class="px-6 py-4 max-w-[50px] overflow-ellipsis overflow-hidden">
-                                    Temmies Sammmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
-                                </td>
-                                <td class="px-6 py-4" colSpan="3">
-                                    4/7
-                                </td>
-                                <td class="px-6 py-4 text-3xl" colSpan="3">
-                                    10
-                                    {/* <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a> */}
-                                </td>
-                            </tr>
-
-                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white max-w-[10px]" >
-                                    1
-                                </th>
-                                <td class="px-6 py-4 max-w-[50px]">
-                                    {/* image */}
-                                    <img src="https://marketplace.canva.com/EAFHfL_zPBk/1/0/1600w/canva-yellow-inspiration-modern-instagram-profile-picture-kpZhUIzCx_w.jpg" className="w-[100px] h-[100]"/>
-                                </td>
-                                <td class="px-6 py-4 max-w-[50px] overflow-ellipsis overflow-hidden">
-                                    Temmies Sammmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
-                                </td>
-                                <td class="px-6 py-4" colSpan="3">
-                                    4/7
-                                </td>
-                                <td class="px-6 py-4 text-3xl" colSpan="3">
-                                    10
-                                    {/* <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a> */}
-                                </td>
-                            </tr>
-
-                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white max-w-[10px]" >
-                                    1
-                                </th>
-                                <td class="px-6 py-4 max-w-[50px]">
-                                    {/* image */}
-                                    <img src="https://marketplace.canva.com/EAFHfL_zPBk/1/0/1600w/canva-yellow-inspiration-modern-instagram-profile-picture-kpZhUIzCx_w.jpg" className="w-[100px] h-[100]"/>
-                                </td>
-                                <td class="px-6 py-4 max-w-[50px] overflow-ellipsis overflow-hidden">
-                                    Temmies Sammmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
-                                </td>
-                                <td class="px-6 py-4" colSpan="3">
-                                    4/7
-                                </td>
-                                <td class="px-6 py-4 text-3xl" colSpan="3">
-                                    10
-                                    {/* <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a> */}
-                                </td>
-                            </tr>
+                            <tr className="border-0"><td>
+                                <div className="mt-3"></div>
+                            </td></tr>
                         </tbody>
+                        {tableBodyContent(joiner, setHoverIndex)}
+                        {/* <tbody className="border-opacity-0">
+                            <tr className="border-opacity-0"><td>
+                                <div className="mt-3"></div>
+                            </td></tr>
+
+                            {tableBodyContent(joiner)}
+                        </tbody> */}
                     </table>
                 </div>
-
             </div>
-
         </main>)
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,15 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
+      <div tabindex="0" class="collapse bg-base-200">
+        <div class="collapse-title text-xl font-medium">
+          Focus me to see content
+        </div>
+        <div class="collapse-content">
+          <p>tabindex="0" attribute is necessary to make the div focusable</p>
+        </div>
+      </div>
+      
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
