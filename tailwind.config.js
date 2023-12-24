2b210a068b7d04c05f08d260e24b93d290e35822
Thitare Nimanong <31370613+goodjobguy1234@users.noreply.github.com>
2023-12-24 22:20:24 +0700

    fix text too large

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'myTopHeader': '#fa34f3',
+        'myTopHeaderTextColor': "#00000"
+      },
     },
   },
   plugins: [
