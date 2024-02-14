a6602fb916fa64dca79c87ea5bff14bf98b66ca3
Akexorcist <akexorcist@gmail.com>
2024-02-15 03:08:29 +0700

    Update contestant and total score UIs

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
       colors: {
         'myTopHeader': '#fa34f3',
-        'myTopHeaderTextColor': "#00000"
+        'myTopHeaderTextColor': "#00000",
+        'dark-rose': '#1B02091F'
       },
     },
   },
